b31e0d1d7d569bd22857f0cd886ed4e7a9371a67
Rohil Hatalkar <Rohil Hatalkar>
2017-05-15 13:22:12 -0700

    Committing Correct Python Program

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,17 +1,1 @@
-print ("Hello World")
-
-#a = [1,2,3,4,5,6,7,8,9,10]
-#a = list(range(1,11))
-#print (a)
-
-c = []
-#for i in a:
-for i in range(1,11):
-    if i%2 == 0:
-      c.append(2*i)  
-print(c)
-
-
-##for f in videoFrames():
-##    if f.isValid():
-##        render(f)
+print ("Hello World!")
